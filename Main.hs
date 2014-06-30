71b881cbcda2fbb18d42e6d1fb22c2d811850fa9
Matthew Avant <matthew.avant@gmail.com>
2014-06-30 00:02:13 -0400

    Constructing HTTP request, but authentication fails.

--- Main.hs
+++ Main.hs
@@ -1,24 +1,37 @@
 module PBook where
+import Network.HTTP
 
 type Prediction = String
--- Might be interesting to do this as something like: data Chance = Real a => Percent a | Odds a a | Pval a
-type Chance = Float
-type Date = String
+type Chance = String -- Constrained to be an integer percentage of no more than 3 digits.
+type Date = String 
+type AuthenticityToken = String
+type UUID = String
 
 main = do
   p <- promptPrediction 
   c <- promptChance 
   d <- promptDate
-  postPrediction p c d
+  t <- getAuthenticityToken
+  u <- getPredictionUUID
+  postPrediction p c d t u
 
-postPrediction :: Prediction -> Chance -> Date -> IO ()
-postPrediction p c d = print (p,c,d)
+getAuthenticityToken :: IO AuthenticityToken
+getAuthenticityToken = return "r78doqWWedU/2ElkBctqlUlb7lDzZVgEY0yXkf2fnlw="
+
+getPredictionUUID :: IO UUID
+getPredictionUUID = return "1073f8d7-3600-4722-bd44-1090a887a0aa"
+
+postPrediction :: Prediction -> Chance -> Date -> AuthenticityToken -> UUID -> IO ()
+postPrediction p c d t u = do
+  let req = postRequestWithBody "http://predictionbook.com/predictions/" "application/x-www-form-urlencoded" ("utf8=✓&authenticity_token=" ++ t ++ "&prediction[uuid]=" ++ u ++ "&prediction[description]=" ++ p ++ "&prediction[initial_confidence]=" ++ c ++ "&prediction[deadline_text]=" ++ d )
+  res <- simpleHTTP req 
+  print res
 
 promptPrediction :: IO Prediction
 promptPrediction = putStrLn "What do you think will (or won't) happen?" >> getLine
 
 promptChance :: IO Chance
-promptChance = putStrLn "What's your estimate of this happening (as a percentage)?" >> readLn
+promptChance = putStrLn "What's your estimate of this happening (as a percentage)?" >> getLine
 
 promptDate :: IO Date
 promptDate = putStrLn "When will you know?" >> getLine
